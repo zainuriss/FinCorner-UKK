8bf7c7b147598dac48e1c1e119ab66c8f285cded
FaizTzyy <faiz.zainuri123@gmail.com>
2025-01-23 21:30:49 +0700

    curved section

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -25,17 +25,20 @@
 <body>
     @include('layouts.navigation')
     <div class="w-full h-screen relative">
-        <img class="w-full h-full object-cover" src="https://images.unsplash.com/photo-1536440136628-849c177e76a1?q=80&w=1450&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
-        <div class="absolute inset-0 bg-gray-950 bg-opacity-80 flex flex-col justify-center items-center text-white text-center gap-4">
+        <img class="w-full h-full object-cover" src="https://plus.unsplash.com/premium_photo-1682125771198-f7cbed7cb868?q=80&w=1460&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+        <div class="absolute inset-0 bg-black bg-opacity-80 flex flex-col justify-center items-center text-white text-center gap-4">
             <h1 class="text-5xl font-bold">Welcome</h1>
-            <p class="font-serif text-wrap px-60">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam dolor aperiam excepturi tempore nostrum ea dicta vitae libero porro facere recusandae, architecto, inventore molestias culpa et animi maxime dignissimos impedit?Labore, voluptatum quidem? Incidunt esse soluta quis, totam natus mollitia. Voluptate ducimus odio quas magni atque fuga blanditiis est, vero sed sunt! Impedit, dolore nam? Nulla cumque fugit dignissimos natus!
+            <p class="font-serif text-wrap lg:px-60 px-10">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam dolor aperiam excepturi tempore nostrum ea dicta vitae libero porro facere recusandae, architecto, inventore molestias culpa et animi maxime dignissimos impedit?Labore, voluptatum quidem? Incidunt esse soluta quis, totam natus mollitia.
             </p>
         </div>
     </div>
     
-    <div class="relative">
-        <svg class="absolute bottom-0 w-full h-auto -mt-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+    <div class="relative ">
+        <svg id="curved-outline" class="absolute bottom-0 w-full h-auto" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 325">
+            <path fill="#fff" fill-opacity="1" d="M0,192L60,181.3C120,171,240,149,360,154.7C480,160,600,192,720,192C840,192,960,160,1080,144C1200,128,1320,128,1380,128L1440,128L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
+        </svg>
+        <svg id="curved" class="absolute bottom-0 w-full h-auto" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 319">
             <path fill="#030712" fill-opacity="1" d="M0,192L60,181.3C120,171,240,149,360,154.7C480,160,600,192,720,192C840,192,960,160,1080,144C1200,128,1320,128,1380,128L1440,128L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
         </svg>
     </div>
@@ -44,5 +47,18 @@
         <h1 class="text-5xl text-white font-serif">Coming Soon</h1>
     </div>    
     
+    <div class="relative rotate-180">
+        <svg id="curved-outline" class="absolute bottom-0 w-full h-auto" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 325">
+            <path fill="#fff" fill-opacity="1" d="M0,192L60,181.3C120,171,240,149,360,154.7C480,160,600,192,720,192C840,192,960,160,1080,144C1200,128,1320,128,1380,128L1440,128L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
+        </svg>
+        <svg id="curved" class="absolute bottom-0 w-full h-auto" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 319">
+            <path fill="#030712" fill-opacity="1" d="M0,192L60,181.3C120,171,240,149,360,154.7C480,160,600,192,720,192C840,192,960,160,1080,144C1200,128,1320,128,1380,128L1440,128L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
+        </svg>
+    </div>
+    
+    <div class="bg-black w-full h-screen flex items-center justify-center">
+        <h1 class="text-5xl text-white font-serif">Coming Soon</h1>
+    </div>
+
 </body>
 </html>
